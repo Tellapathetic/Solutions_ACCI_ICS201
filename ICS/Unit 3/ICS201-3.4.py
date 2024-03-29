235d49605a0e5b8b08ca6d8acd28fde74e718d80
Tellapathetic <105130921+Tellapathetic@users.noreply.github.com>
2024-03-28 21:44:54 -0400

    Delete ICS directory

--- ICS/Unit 3/ICS201-3.4.py
+++
@@ -1,9 +0,0 @@
-# Given N numbers: the first number in the input is N, after that N integers are given. Count the number of zeros among the given integers and print it.
-# Empty List to store # of 0s
-zerolist = []
-# For loop for N length
-for i in range(int(input("In: "))):
-  # Append the number of 0s in each input into the empty list.
-  zerolist.append(input(f"In #{i+1}: ").count("0"))
-# Print the sum of the list
-print(sum(zerolist))
